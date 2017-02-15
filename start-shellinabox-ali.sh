c6a2512e6719f16256f793bb185e1fa8b1ab9d35
Codecolar <xiaolang5293@gmail.com>
2017-02-15 15:27:50 +0800

    Update start-shellinabox-ali.sh

--- start-shellinabox-ali.sh
+++ start-shellinabox-ali.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 killall shellinaboxd
-# shellinaboxd -b -c /tmp -p 4200 -s /:SSH:59.110.142.10 
-shellinaboxd -b -t -p 62222 -s /:SSH:59.110.142.10 
+shellinaboxd -b -c /tmp -p 4200 -s /:SSH:59.110.142.10 
+# shellinaboxd -b -t -p 62222 -s /:SSH:59.110.142.10 
